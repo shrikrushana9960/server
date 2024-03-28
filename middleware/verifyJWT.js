8cfcc1555b42a1876814091c81e9caf3eb021cfe
Shri <papupingale123@gmail.com>
2024-03-29 00:38:59 +0530

    fix

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,23 +1,37 @@
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.config');
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.config");
 
 const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
 
-    const token = req.headers.authorization || req.cookies.access_token;
-    
-    if (!token) {
-        return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
+  // Check if Authorization header exists and starts with 'Bearer '
+  if (!req?.cookies?.access_token) {
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: No token provided" });
+    }
+  }
+
+  // Extract the token from the Authorization header
+  let token = authHeader.split(" ")[1] || req.cookies.access_token;
+
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized: No token provided" });
+  }
+
+  jwt.verify(token, authConfig.secret, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized: Invalid token" });
     }
 
-    jwt.verify(token, authConfig.secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ success: false, message: 'Unauthorized: Invalid token' });
-        }
-
-
-        req.user = decoded;
-        next();
-    });
+    req.user = decoded;
+    next();
+  });
 };
 
 module.exports = verifyJWT;
